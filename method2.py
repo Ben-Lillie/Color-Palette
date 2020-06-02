4c07ad8d77408cd21b4ecf85f56446cc625b0a77
Ben Lillie <ben@lillie.me.uk>
2020-06-02 13:37:40 +0100

    2nd Method

---
+++ method2.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from skimage import io
+
+img = io.imread('https://i.stack.imgur.com/DNM65.png')[:, :, :-1]
+
+average = img.mean(axis=0).mean(axis=0)
+
+pixels = np.float32(img.reshape(-1, 3))
+
+n_colors = 5
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+flags = cv2.KMEANS_RANDOM_CENTERS
+
+_, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+_, counts = np.unique(labels, return_counts=True)
+
+dominant = palette[np.argmax(counts)]
